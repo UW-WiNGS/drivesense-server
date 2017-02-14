c79472bec8601297b6e3c0740de530f2e1e31033
pddenhar@gmail.com <pddenhar@gmail.com>
2017-02-14 16:33:39 -0600

    Oops, don't delete those columns

--- database/schema/trip.sql
+++ database/schema/trip.sql
@@ -4,6 +4,8 @@ CREATE TABLE `trip` (
   `userid` int(10) UNSIGNED NOT NULL,
   `deviceid` varchar(100) DEFAULT NULL,
   `model` varchar(100) DEFAULT NULL,
+  `starttime` bigint(20) UNSIGNED DEFAULT NULL,
+  `endtime` bigint(20) UNSIGNED DEFAULT NULL,
   `distance` decimal(16,8) DEFAULT NULL,
   `status` int(11) DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
